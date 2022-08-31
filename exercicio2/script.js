59f4e20f27847012d44225b6ba22304919d49cec
Byenktrovik <byenktrovik@gmail.com>
2022-08-31 18:13:01 -0300

    exercicios3108

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
+
+
+/*a) Remova o excesso de espaços no final da string;*/
+
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+
+
+console.log(minhaString.length);
+console.log(minhaString.trim().length);
+//trocar a string  
+console.log(minhaString.replace('________', 'sticioso') )
